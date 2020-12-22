9e0061d27b50797606c231661678c62e1bbf20e9
William Steward <alexsteward98@gmail.com>
2020-12-22 15:40:27 -0600

    added variable canvas size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,42 +1,54 @@
-var grid_width = 16
-var grid_height = 16
+function resizeCSSGrid(width, height, gridContainer){
+    gridContainer.style.gridTemplateColumns = `repeat(${width}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${height}, 1fr)`
+    
+    return
+}
 
 function drawGrid(width, height){
     console.log(`Pixels to be draw: ${width*height}`)
 
-    for(let i = 0; i < (grid_height*grid_height) ; i++){
+    const canvas = document.querySelector(".etch-a-sketch")
+    resizeCSSGrid(width, height, canvas)
+    // create the pixels
+    for(let i = 0; i < (width*height) ; i++){
         const pixel = document.createElement("div")
-        pixel.classList.add("pixel")
-        document.querySelector(".etch-a-sketch").appendChild(pixel)
+
+        pixel.classList.add("pixel") //add the basic pixel styling to the pixels
+        pixel.addEventListener('mouseover', (e) =>{
+                pixel.classList.add("active-pixel") //add the mouseover even to the pixels
+        })
+
+        canvas.appendChild(pixel) //add the pixels to the dom
     }
 
-    let pixels = document.querySelectorAll(".pixel")
-    pixels.forEach(pixel => pixel.addEventListener('mouseover', (e) =>{
-        pixel.classList.add("active-pixel")
-    }))
+    return
 }
 
 function clearGrid(){
     const canvas = document.querySelector(".etch-a-sketch")
-
     while (canvas.firstChild){
         canvas.removeChild(canvas.firstChild)
     }
-    
+
+    return
 }
+
+
 
 function newGrid(){
     clearGrid()
-    drawGrid(grid_height, grid_width)
+    
+    const canvasWidth = document.getElementById('width').value
+    const canvasHeight = document.getElementById('height').value
+    console.log(canvasHeight, canvasWidth)
+
+    drawGrid(canvasHeight, canvasWidth)
+
+    return
 }
 
-const clearButton = document.getElementById("clear")
+const clearButton = document.getElementById("new-canvas")
 clearButton.addEventListener('click', newGrid)
 
 newGrid()
-
-
-
-
-
-
