ba2148c4020368768f19d1ba46c1362e3a7e3d97
William Steward <alexsteward98@gmail.com>
2020-12-22 14:59:29 -0600

    added etch a sketch logic refacotred into functions

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,42 @@
-const grid_width = 16
-const grid_height = 16
+var grid_width = 16
+var grid_height = 16
 
-for(let i = 0; i < (grid_height*grid_height) ; i++){
-    const pixel = document.createElement("div")
-    pixel.classList.add("pixel")
-    document.querySelector(".etch-a-sketch").appendChild(pixel)
+function drawGrid(width, height){
+    console.log(`Pixels to be draw: ${width*height}`)
+
+    for(let i = 0; i < (grid_height*grid_height) ; i++){
+        const pixel = document.createElement("div")
+        pixel.classList.add("pixel")
+        document.querySelector(".etch-a-sketch").appendChild(pixel)
+    }
+
+    let pixels = document.querySelectorAll(".pixel")
+    pixels.forEach(pixel => pixel.addEventListener('mouseover', (e) =>{
+        pixel.classList.add("active-pixel")
+    }))
 }
+
+function clearGrid(){
+    const canvas = document.querySelector(".etch-a-sketch")
+
+    while (canvas.firstChild){
+        canvas.removeChild(canvas.firstChild)
+    }
+    
+}
+
+function newGrid(){
+    clearGrid()
+    drawGrid(grid_height, grid_width)
+}
+
+const clearButton = document.getElementById("clear")
+clearButton.addEventListener('click', newGrid)
+
+newGrid()
+
+
+
+
+
+
